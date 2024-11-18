e9b47dfc3ac49a33fa9974882455d6af231d8f5f
LittleW <w-army@hotmail.co.th>
2024-11-18 23:05:07 +0700

    sakfjdhnaskl

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/LittleW-Front-End-Project-11/todolist/'
+  plugins: [react()]
 })
